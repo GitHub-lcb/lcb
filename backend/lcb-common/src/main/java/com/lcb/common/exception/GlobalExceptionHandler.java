410801e2f39b58cba7981f10962ccba8db816100
chenbo.li <chenbo.li@zhaogang.com>
2026-05-08 14:27:49 +0800

    fix: add slf4j logging to global exception handler

--- backend/lcb-common/src/main/java/com/lcb/common/exception/GlobalExceptionHandler.java
+++ backend/lcb-common/src/main/java/com/lcb/common/exception/GlobalExceptionHandler.java
@@ -1,19 +1,23 @@
 package com.lcb.common.exception;
 
 import com.lcb.common.core.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ServiceException.class)
     public Result<Void> handleServiceException(ServiceException e) {
+        log.warn("业务异常: {}", e.getMessage());
         return Result.fail(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public Result<Void> handleException(Exception e) {
+        log.error("系统异常", e);
         return Result.fail("系统繁忙，请稍后重试");
     }
 }
